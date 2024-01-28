34f8790f50bdace5267e0b4c6ee2271700c6a0e9
강예은 <yeaeun411@naver.com>
2024-01-29 02:07:55 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -1,14 +1,16 @@
 class Solution {
     public int solution(int n) {
-        long answer = 1;
+        long[] dp = new long[n + 1];
 
-        // Cn = (1/(n+1)) * (2n)! / (n! * n!)
-        for (int i = 0; i < n; i++) {
-            answer *= 2 * (2 * i + 1);
-            answer /= (i + 2);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 0;
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
         }
 
-        return (int)answer;
-
+        return (int)dp[n];
     }
 }
