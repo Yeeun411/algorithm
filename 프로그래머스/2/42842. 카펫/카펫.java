c19f179bba284d63389f5abe011acc317e14873f
강예은 <yeaeun411@naver.com>
2024-02-19 19:09:01 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        for (int i = 1; i <= Math.sqrt(yellow); i++) {
+            if (yellow % i == 0) {
+                int y = i;
+                int x = yellow / i;
+                if ((x + 2) * (y + 2) - yellow == brown) {
+                    return new int[] {x + 2, y + 2};
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
