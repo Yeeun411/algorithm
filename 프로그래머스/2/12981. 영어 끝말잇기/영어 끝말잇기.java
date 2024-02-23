253262ffa98c44b48f8d346e19c86d2bc669b4ab
강예은 <yeaeun411@naver.com>
2024-02-23 21:07:52 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        Set<String> usedWords = new HashSet<>();
+        char lastChar = words[0].charAt(0);  // 초기 값 설정, 실제로는 사용되지 않음
+
+        for (int i = 0; i < words.length; i++) {
+            if (usedWords.contains(words[i]) || words[i].charAt(0) != lastChar) {
+                // (i % n) + 1은 탈락하는 플레이어 번호를, (i / n) + 1은 탈락하는 턴을 계산
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            }
+
+            usedWords.add(words[i]);  // 현재 단어를 사용된 단어 집합에 추가
+            lastChar = words[i].charAt(words[i].length() - 1);  // 마지막 문자 업데이트
+        }
+
+        return new int[]{0, 0};
+    }
+}
