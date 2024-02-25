4210d0d47aa18117f60e508869609e4cbeae0507
강예은 <yeaeun411@naver.com>
2024-02-25 22:29:17 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,19 +1,19 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        Set<String> usedWords = new HashSet<>();
-        char lastChar = words[0].charAt(0);
-        
+        Set<String> prev = new HashSet<>();
+        String word = words[0];
+        prev.add(word);
 
-        for (int i = 0; i < words.length; i++) {
-            if (usedWords.contains(words[i]) || words[i].charAt(0) != lastChar) {
-                // (i % n) + 1은 탈락하는 플레이어 번호를, (i / n) + 1은 탈락하는 턴을 계산
+        for (int i = 1; i < words.length; i++) {
+            if (prev.contains(words[i]) || word.charAt(word.length() - 1) != words[i].charAt(0)) {
                 return new int[]{(i % n) + 1, (i / n) + 1};
             }
 
-            usedWords.add(words[i]);  // 현재 단어를 사용된 단어 집합에 추가
-            lastChar = words[i].charAt(words[i].length() - 1);  // 마지막 문자 업데이트
+            prev.add(words[i]);
+            word = words[i];
         }
+
 
         return new int[]{0, 0};
     }
